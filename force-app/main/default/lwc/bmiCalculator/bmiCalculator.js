0c2439d5c28eae3aae7e6012e6cec19062262c19
Tasnim DRIDI <tasnim.dridi@cognizant.com>
2024-05-27 16:19:38 +0200

    jsModif

--- force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
+++ force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -1,3 +1,24 @@
 import { LightningElement } from 'lwc';
 
-export default class BmiCalculator extends LightningElement {}
+export default class BmiCalculator extends LightningElement {
+
+height='';
+weight='';
+inputHandler(event){
+    const {name,value}= event.target;
+    if(name==="height"){
+        this.height=value;
+    }
+    if(name==="weight"){
+        this.weight=value;
+    }
+
+
+}
+submitHandler(event){
+    event.preventDefault();
+    console.log("height" ,this.height);
+    console.log("weight" ,this.weight);
+}
+
+}
